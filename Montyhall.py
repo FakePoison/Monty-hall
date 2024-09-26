c033ff54e2f1cf450f197cf6cff13e31e2a1c528
Charles Dannen <cdannen@protonmail.com>
2024-09-26 16:03:11 -0700

    Added iterator to test over a large sample size.

--- Montyhall.py
+++ Montyhall.py
@@ -1,20 +1,34 @@
 import random
 
+
 #a list of doors to choose from, 2 winners 1 loser
 #0 is a loser 1 is a winner
 doorslist = [0, 0, 1]
 #the order of winners and losers should not matter since the doors will be selected randomly anyway
 #TODO randomize door list as well to prove this
 
+#function to run iterations of the problem
+def montyiter(repeats):
+    i = repeats
+    x = 0
+    while i > 0:
+        dlist = doorslist.copy()
 #select a door
-selecteddoor = random.choice(doorslist)
-print(selecteddoor)
+        selecteddoor = random.choice(doorslist)
 
 #next, remove the door from the list
-doorslist.remove(selecteddoor)
+        dlist.remove(selecteddoor)
 
 #finally remove a losing door from the list, leaving one option
-doorslist.remove(0)
+        dlist.remove(0)
 
 #show the selected door and what was behind the remaining door
-print(f'The chosen door was {selecteddoor} and the last door was {doorslist[0]}')
+#print(f'The chosen door was {selecteddoor} and the last door was {doorslist[0]}')
+        if selecteddoor == 1:
+            x += 1
+        
+        i -= 1
+    
+    print(f'Guessed correct {x} out of {repeats} times')
+
+montyiter(1000)
